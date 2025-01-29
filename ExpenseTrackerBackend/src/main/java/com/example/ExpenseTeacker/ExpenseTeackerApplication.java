0f601fe99b42cfd8213a8cfa86f0382692a74766
Pruthviraj Jadhav <pruthviraj.jadhav@nitorinfotech.com>
2025-01-29 11:47:50 +0530

    first commit

---
+++ ExpenseTrackerBackend/src/main/java/com/example/ExpenseTeacker/ExpenseTeackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExpenseTeacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTeackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTeackerApplication.class, args);
+	}
+
+}
---
+++ ExpenseTrackerBackend/src/test/java/com/example/ExpenseTeacker/ExpenseTeackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpenseTeacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTeackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
