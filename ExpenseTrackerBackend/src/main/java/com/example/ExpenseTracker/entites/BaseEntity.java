67abeb6f7df1d814f8531d26027f4f11a7434007
PruthvirajNitor <pruthviraj.jadhav@nitorinfotech.com>
2025-02-11 11:50:09 +0530

    added base entity, and edited others (#3)

---
+++ ExpenseTrackerBackend/src/main/java/com/example/ExpenseTracker/entites/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.example.ExpenseTracker.entites;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto increment of id
+    private long id;
+
+}
--- ExpenseTrackerBackend/src/main/java/com/example/ExpenseTracker/entites/Expense.java
+++ ExpenseTrackerBackend/src/main/java/com/example/ExpenseTracker/entites/Expense.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
@@ -15,20 +17,25 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Table(name = "expenses")
-public class Expense {
+public class Expense extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonProperty //it forces id to included in json
-    private long id;
 
     @NotEmpty(message = "Title cannot be empty or null") //it is used so that title can nor be null nor be empty ie ""
     @Size(max = 255,message = "title cannot be more than 255 characters")
     private String title;
 
     @NotNull
-    private String amount;
+    @Positive(message = "Amount cannot be negative")
+    private Double amount;
+
+    @NotNull
+    @Positive(message = "Available Balance cannot be negative")
+    private Double availBal; //Used to set the budget
+
+    @NotNull
+    private Double lowerLimit;
 
     @Enumerated(EnumType.STRING) //saved in database as a string and not ordinal
     private Category category;
